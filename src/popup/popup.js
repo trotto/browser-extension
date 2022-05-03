486b77f4e62c13d49fdcae4b9092f6313c64fd7f
Brad Taylor <bradtaylorsf@gmail.com>
2022-05-02 21:13:11 -0700

    added POC logic for creating go link from the popup

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -7,7 +7,40 @@ export class Popup {
   }
 
   run() {
-    this.api.tabs.create({ url: getInstanceUrl() })
-        .then(() => window.close());
+    let queryOptions = { active: true, currentWindow: true };
+    this.api.tabs.query(queryOptions)
+      .then(([tab]) => document.getElementById('currentUrl').value = tab.url);
+    
+    document.getElementById('submit').onclick = () => {
+      fetch('https://trot.to/_/api/links', {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+          Origin: 'api.trot.to',
+        },
+        body: JSON.stringify({
+          "namespace": 'go',
+          "shortpath": document.getElementById('shortUrl').value,
+          "destination": document.getElementById('currentUrl').value,
+          "owner": 'brad@trot.io',
+          "visits_count": 0,
+        }),
+      })
+      .then(response => {
+        console.log(response.json());
+      })
+      .then(data => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+    }
+    
+    
+    
+    // this.api.tabs.create({ url: getInstanceUrl() })
+    //     .then(() => window.close());
   }
 }
